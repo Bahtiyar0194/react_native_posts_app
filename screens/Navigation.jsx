c59daaf96881cbd9bea1cfc944e9e98d4adb2fab
Baktiyar <bahtiyar_0194@mail.ru>
2022-11-15 17:32:05 +0500

    Initial commit

--- screens/Navigation.jsx
+++ screens/Navigation.jsx
@@ -8,7 +8,12 @@ const Stack = createNativeStackNavigator();
 export const Navigation = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#2F3136',
+                },
+                headerTintColor: '#fff'
+            }}>
                 <Stack.Screen name='Home' component={HomeScreen} options={{ title: "Новости" }} />
                 <Stack.Screen name='FullPost' component={FullPostScreen} options={{ title: "Новость" }} />
             </Stack.Navigator>
